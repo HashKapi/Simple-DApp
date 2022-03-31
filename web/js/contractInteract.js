9004257ecc1a759ab1f16b4bd5d66df82de47441
KennyAckermanHub <hashim.hirji@gmail.com>
2022-03-31 15:57:53 -0400

    Managed to perform transactions

--- web/js/contractInteract.js
+++ web/js/contractInteract.js
@@ -1,9 +1,86 @@
-web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"))
+// const {abi} = require('js/contractAbi')
+
+if(typeof web3 != "undefined"){
+    web3 = new Web3(web3.currentProvider)
+}else{
+    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"))
+}
+
+
 
 var version = web3.version
 
 console.log('Printing out version ' + version)
 
+
+var contractAddress = '0x9537d614c3365Cc88b24326f600C79e9ABcd51a0'
+var ApprovalContract = new web3.eth.Contract(abi, contractAddress)
+
+console.log(ApprovalContract)
+
+$('#contract-form').submit(function (){
+    event.preventDefault()
+    var fromAddress = $("#fromAddress").val()
+    var toAddress = $("#toAddress").val()
+    var amount = $("#amount").val()
+
+    if (web3.utils.isAddress(fromAddress) != true){
+        alert('invalid ethereum from address')
+        return
+    }
+
+    if (web3.utils.isAddress(toAddress) != true){
+        alert("invalid ethereum to address")
+        return
+    }
+
+    if (amount <= 0){
+        alert("you must send more than 0 ETH")
+        return
+    }
+
+    ApprovalContract.methods.deposit(toAddress).send(
+        {from: fromAddress, gas: 100000, value: web3.utils.toWei(amount,'ether')},
+        function(error, result){
+            if(error){
+                console.log('error: ' + error)
+                $('#deposit-result').html('error: <b>' + error + '</b>')
+            }else{
+                $('#deposit-result').html('Success TX: <b>' + result + '</b>')
+            }
+        }
+    )
+})
+
+$('#get-balance-form').submit(function() {
+    event.preventDefault()
+
+    web3.eth.getBalance(contractAddress, 
+        function(error, result){
+            if (error){
+                console.log('error: ' + error)
+            }else{
+                console.log('balance: ' + result)
+                $('#the-balance').html('<b>Current Balance: </b>' + web3.utils.fromWei(result))
+            }
+        }
+    )
+})
+
+$('#approve-form').submit(function(){
+    event.preventDefault()
+
+    ApprovalContract.methods.approve().call({from: '0xa8Ab4262dD72071d67D2BfeDCA3a95Bf45E9Aa24'},
+        function(error, result){
+            if(error){
+                console.log('error: ' + error)
+            }else{
+                console.log('result: ' + JSON.stringify(result))
+                $('#approval-display').html('Transaction approved. TX: <b>' + result + '<b>')
+            }
+        }
+    )
+})
 
 
 
